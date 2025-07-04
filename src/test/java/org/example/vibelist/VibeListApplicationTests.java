fbcff88e090874025f7cac4de0b82f697d10e6df
taco-recipe <taco9590@gmail.com>
2025-07-04 14:55:53 +0900

    🎉 초기 프로젝트 커밋

---
+++ src/test/java/org/example/vibelist/VibeListApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.vibelist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
