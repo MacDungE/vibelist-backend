617c7bf5701fa01c4e34fa77ee81e27274c2745f
jongmyeong <polcaneli@gmail.com>
2025-07-07 12:41:51 +0900

    ✨feat: properties 및 환경변수 headth check 추가(#17)

---
+++ src/main/java/org/example/vibelist/auth/controller/AuthController.java
@@ -0,0 +1,7 @@
+package org.example.vibelist.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+}
--- src/main/java/org/example/vibelist/global/config/ProdSecurityConfig.java
+++ src/main/java/org/example/vibelist/global/config/ProdSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -18,11 +21,21 @@ public class ProdSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(ahr ->
-                        ahr.requestMatchers("/**").permitAll());
+                        ahr.requestMatchers("/v1/public/**").permitAll()
+                           .requestMatchers("/v1/auth/**").permitAll()
+                           .requestMatchers("/health", "/actuator/health").permitAll()
+                           .anyRequest().authenticated());
+        http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.logout(withDefaults());
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
