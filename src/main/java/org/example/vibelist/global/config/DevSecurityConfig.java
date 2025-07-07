03255a063a484c5e9320338ce0898d32726bd469
jongmyeong <polcaneli@gmail.com>
2025-07-07 11:18:54 +0900

    ✨feat: spring security 초기세팅 (#17)

---
+++ src/main/java/org/example/vibelist/global/config/DevSecurityConfig.java
@@ -0,0 +1,28 @@
+package org.example.vibelist.global.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@Profile("dev")
+public class DevSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(ahr ->
+                        ahr.requestMatchers("/**").permitAll());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+}
