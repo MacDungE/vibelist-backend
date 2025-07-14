4c280d6441c6a039845dbc6bfe004304078c2e08
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2025-07-14 15:52:10 +0900

    🐛 fix: 로그 테스트

--- src/main/java/org/example/vibelist/VibeListApplication.java
+++ src/main/java/org/example/vibelist/VibeListApplication.java
@@ -1,14 +1,17 @@
 package org.example.vibelist;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class VibeListApplication {
 
     public static void main(String[] args) {
+        log.info("🔥 Application is starting...");
         SpringApplication.run(VibeListApplication.class, args);
     }
 
