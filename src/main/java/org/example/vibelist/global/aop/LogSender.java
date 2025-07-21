5e1e836a77e5eb04c0a58786b74d1919137badb7
sung_1 <moonjs1011@naver.com>
2025-07-21 14:59:31 +0900

    ♻️ Feat: 유저의 Login/Logout 이벤트 감지 로그 발생 [#71]
    Logstash로 넘어갈 때 json형태가 아닌, string형태로 넘어가는 문제를 해결하기 위해 넘기기전 전처리 작업을 해줬습니다.

--- src/main/java/org/example/vibelist/global/aop/LogSender.java
+++ src/main/java/org/example/vibelist/global/aop/LogSender.java
@@ -5,12 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.argument.StructuredArguments;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class LogSender {
     private final ObjectMapper objectMapper ;
+    private static final Logger userLogger = LoggerFactory.getLogger("user-log"); //LOGSTASH에만 전송되는 로그들을 관리
     public LogSender(){
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule()); // Jackson에서 직렬화 등록, 기본적으로 지원을 안함
@@ -21,7 +25,14 @@ public class LogSender {
     public void send(UserLog logData){
         try{
             String jsonLog = objectMapper.writeValueAsString(logData);
-            log.info(jsonLog);
+            userLogger.info("User Action Log", //log 이름
+                    StructuredArguments.keyValue("userId", logData.getUserId()), //json으로 파싱하기 위한 전처리 단계
+                    StructuredArguments.keyValue("ip", logData.getIp()),
+                    StructuredArguments.keyValue("eventType", logData.getEventType()),
+                    StructuredArguments.keyValue("domain", logData.getDomain()),
+                    StructuredArguments.keyValue("timestamp", logData.getTimestamp().toString()),
+                    StructuredArguments.keyValue("message", logData.getMessage())
+            );
         }
         catch(JsonProcessingException e){
             e.printStackTrace();
