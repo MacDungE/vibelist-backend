1c38a883ba18f7da003c97f40449eadd9ca5ead0
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2025-07-22 09:48:39 +0900

    🐛 fix: log 테스트

--- src/main/java/org/example/vibelist/VibeListApplication.java
+++ src/main/java/org/example/vibelist/VibeListApplication.java
@@ -11,11 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class VibeListApplication {
 
     public static void main(String[] args) {
-        log.info("[info]log test");
-        log.error("[error]log test");
-        log.warn("[warn]log test");
-        log.trace("[trace]log test");
-
         SpringApplication.run(VibeListApplication.class, args);
     }
 
