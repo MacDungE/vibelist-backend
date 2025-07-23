e52bb7792cd306351b3355b8f35543af2d631a62
sung_1 <moonjs1011@naver.com>
2025-07-23 17:39:44 +0900

    ✨ Feat : UserLog에 Duration field 추가[#71]
    컨트롤러 응답시간을 측정하여 넣어주는 로직 추가/ Duration field를 추가 / logback-spring.xml를 추가했습니다.

--- src/main/java/org/example/vibelist/global/aop/UserLog.java
+++ src/main/java/org/example/vibelist/global/aop/UserLog.java
@@ -14,5 +14,6 @@ public class UserLog {
     private String domain; // ex: user,post, etc
     private String api;// ex: api/v1/post
     private String requestBody;
+    private Long duration;
     private LocalDateTime timestamp;
 }
